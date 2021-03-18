63eb1a4c87185c54283fd03da308f33417ea9926
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-03-18 21:57:36 +0100

    Update eventlog-trigger.ps1

--- eventlog-trigger.ps1
+++ eventlog-trigger.ps1
@@ -1,6 +1,14 @@
+# Documentation register-objectevent
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/register-objectevent?view=powershell-5.1
-# ObjectEvent, WMIEvent, EngineEvent
+# WMIEvent: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/register-wmievent?view=powershell-5.1
+# EngineEvent: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/register-engineevent?view=powershell-5.1
 
+# This Script registers a new Objectevent 
+# When a special Event Log entry occures, a action is started
+
+##########################################################################################################################################
+
+##########################################################################################################################################
 # EventLog Name
 $Name = 'Application'
 
@@ -20,12 +28,15 @@ $Action = {
   
   }
 }
+##########################################################################################################################################
 
 # Abo Events with type "EntryWritten"
 # For different Events safe in different variables / und SourceIdentifiers
 # Help: InputObject => What Log - EventName => Which Event to abo - SourceIdentifier => Abo-name - Action => Do what?
 
 $job1 = Register-ObjectEvent -InputObject $LogInstance -EventName EntryWritten -SourceIdentifier 'MyEventHandler' -Action $Action
+
+##########################################################################################################################################
 
 <#
 # To stop Abo use this line
@@ -36,6 +47,8 @@ Unregister-Event -SourceIdentifier 'MeinEventHandler'
 # To register new Eventlog (only needed once) use this
 New-EventLog -LogName Application -Source ‘MyLog’
 #>
+
+##########################################################################################################################################
 
 # Write new Eventlog to test Eventlog trigger
 write-eventlog -entrytype "Warning" -logname "application" -eventID 999 -Source 'MyLog' -Category 0 -Message "Test"
