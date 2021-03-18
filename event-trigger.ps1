4bc24d9164f073e6fcdcdd6ecf78ebf9586a834c
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-03-18 17:52:55 +0100

    Create event-trigger.ps1

---
+++ event-trigger.ps1
@@ -0,0 +1,36 @@
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/register-objectevent?view=powershell-5.1
+
+# Name des EventLogs
+$Name = 'Application'
+
+# Eventlog Instanz holen anhand von Name
+$LogInstance = [System.Diagnostics.EventLog]$Name
+
+# Was soll getan werden?
+$Aktion = {
+# Event aus der Historie holen
+$eintrag = $event.SourceEventArgs.Entry
+
+# eingehende Einträge filtern
+if ($eintrag.EventId -eq 0 -and $eintrag.Source -eq 'DockerService') 
+{
+Write-Host "Achtung! Event hat stattgefunden. Es ist an der Zeit DINGE zu tun"
+}
+
+}
+
+#InputObject = Was/welches Log soll ich überwachen?
+#EventName = Welche Art von Event will ich Abonieren?
+#SourceIdentifier = Wie soll dein 'Abo' heißen?
+#Aktion = Selbsterklärend
+
+# Events vom typ "EntryWritten" abonnieren
+# Willst du verschiedene Events abonnieren speichere einfach in verschiedenen Variablen und verschiedenen SourceIdentifier
+$job1 = Register-ObjectEvent -InputObject $LogInstance -EventName EntryWritten -SourceIdentifier 'MeinEventHandler' -Action $Aktion
+
+#Und einmal Abo beenden
+#Unregister-Event -SourceIdentifier 'MeinEventHandler'
+
+#ObjectEvent
+#WMIEvent
+#EngineEvent
