1e7196fd695bad0cb0ace3a38cd5b7ba96e35d3f
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-04-29 09:52:36 +0200

    Update WLAN-Info.ps1

--- WLAN-Info.ps1
+++ WLAN-Info.ps1
@@ -1,20 +1,21 @@
   Write-Verbose -Message "netsh.exe command to get wirelss profile info" -Verbose
   $output = netsh.exe wlan show interfaces
 
-  Write-Verbose -Message "  # Name" -Verbose
   $Name_line = $output | Select-String -Pattern 'Name'
   $Name = ($Name_line -split ":")[-1].Trim()
-  $Name 
+  Write-Verbose -Message "Name: $Name " -Verbose
 
-  Write-Verbose -Message "# Description" -Verbose
+  $Profile_line = $output | Select-String -Pattern 'Profil'
+  $Profile = ($Profile_line -split ":")[-1].Trim()
+  Write-Verbose -Message "Profile: $Profile" -Verbose
+
   $Description_line = $output | Select-String -Pattern 'Beschreibung'
   $Adaptor = ($Description_line -split ":")[-1].Trim()
-  $Adaptor
+  Write-Verbose -Message "Description: $Adaptor" -Verbose
 
-  Write-Verbose -Message "# GUID" -Verbose
   $GUID_line = $output | Select-String -Pattern 'GUID'
   $GUID = ($GUID_line -split ":")[-1].Trim()
-  $GUID 
+  Write-Verbose -Message "GUID: $GUID" -Verbose 
 
 <#
   Write-Verbose -Message "# State" -Verbose
@@ -23,63 +24,49 @@
   $State
 #>
 
-    Write-Verbose -Message "# SSID" -Verbose
+    Write-Verbose -Message "SSID: $SSIDText" -Verbose
     $SSID_line = $output | Select-String 'SSID'| select -First 1
-    $SSIDText = ($SSID_line -split ":")[-1].Trim()
-    $SSIDText 
+    $SSIDText = ($SSID_line -split ":")[-1].Trim()    
 
-    Write-Verbose -Message "# BSSID" -Verbose
+    Write-Verbose -Message "BSSID: $BSSIDText" -Verbose
     $BSSID_line = $output | Select-String -Pattern 'BSSID'
     $BSSIDText = ($BSSID_line -split ":", 2)[-1].Trim()
-    $BSSIDText
 
-    Write-Verbose -Message "# NetworkType" -Verbose
+    Write-Verbose -Message "NetworkType: $NetworkType" -Verbose
     $NetworkType_line = $output | Select-String -Pattern 'Netzwerktyp'
     $NetworkType = ($NetworkType_line -split ":")[-1].Trim()
-    $NetworkType
 
-    Write-Verbose -Message "# Authentication" -Verbose
     $Authentication_line = $output | Select-String -Pattern 'Authentifizierung'
     $Auth = ($Authentication_line -split ":")[-1].Trim()
-    $Auth
-
-    Write-Verbose -Message "# Cipher" -Verbose
+    Write-Verbose -Message "Authentication: $Auth" -Verbose
+    
     $Cipher_line = $output | Select-String -Pattern 'Funktyp'
     $CipherText = ($Cipher_line -split ":")[-1].Trim()
-    $CipherText
-
-
-    Write-Verbose -Message "# Connection mode" -Verbose
+    Write-Verbose -Message "Cipher: $CipherText" -Verbose
+    
     $Connection_line = $output | Select-String -Pattern 'Verbindungsmodus'
     $Connection = ($Connection_line -split ":")[-1].Trim()
-    $Connection 
-
-    Write-Verbose -Message "# Channel" -Verbose
+    Write-Verbose -Message "Connection mode: $Connection" -Verbose
+     
     $Channel_line = $output | Select-String -Pattern 'Kanal'
     $Chan = ($Channel_line -split ":")[-1].Trim()
-    $Chan
-
-    Write-Verbose -Message "# Receive Rate" -Verbose
+    Write-Verbose -Message "Channel: $Chan" -Verbose
+    
     $RecRate_line = $output | Select-String -Pattern 'Empfangsrate'
     $RxRate = ($RecRate_line -split ":")[-1].Trim()
-    $RxRate
-	
-	Write-Verbose -Message "# Transmit Rate" -Verbose
+    Write-Verbose -Message "Receive Rate: $RxRate" -Verbose
+    
     $TransRate_line = $output | Select-String -Pattern 'bertragungsrate'
     $TxRate = ($TransRate_line -split ":")[-1].Trim()
-    $TxRate
-
-    Write-Verbose -Message "# Signal (%)" -Verbose
+	Write-Verbose -Message "Transmit Rate: $TxRate" -Verbose
+    
     $SignalLevelPercent_line = $output | Select-String -Pattern 'Signal'
     $SignalLevelPercent = ($SignalLevelPercent_line -split ":")[-1].Trim()	
-    $SignalLevelPercent
+    Write-Verbose -Message "Signal (%): $SignalLevelPercent" -Verbose
+    
 
-	Write-Verbose -Message "# Signal (dBm)" -Verbose
     $SignalLevelPercent_trimmed = $SignalLevelPercent.TrimEnd('%')
     $dBmSig = (([int]$SignalLevelPercent_trimmed)/2) - 100
-	$dBmSig
-
-    Write-Verbose -Message "# Profile" -Verbose
-    $Profile_line = $output | Select-String -Pattern 'Profil'
-    $Profile = ($Profile_line -split ":")[-1].Trim()
-    $Profile 
+	Write-Verbose -Message "Signal (dBm): $dBmSig" -Verbose
+	
+    
