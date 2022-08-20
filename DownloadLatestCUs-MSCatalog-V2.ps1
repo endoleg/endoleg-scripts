4747ca3caed730603a1ac885cb8dd5da7144902e
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2022-08-20 19:20:25 +0200

    Update DownloadLatestCUs-MSCatalog-V2.ps1

--- DownloadLatestCUs-MSCatalog-V2.ps1
+++ DownloadLatestCUs-MSCatalog-V2.ps1
@@ -5,7 +5,6 @@ $winver=$(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVer
 $winver=(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').DisplayVersion
 Write-verbose -message "Windows Version: $winver" -verbose
 
-
 #########################################################################################
 
 #out-grid
@@ -13,25 +12,18 @@ $Products=@()
 $Products+=@{Product="Windows Server 2022" ;SearchString="Cumulative Update for Windows Server 2022 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2022 for x64-based Systems"           ; ID="Windows Server 2022"}
 $Products+=@{Product="Windows Server 2019" ;SearchString="Cumulative Update for Windows Server 2019 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2019 for x64-based Systems"           ; ID="Windows Server 2019"}
 $Products+=@{Product="Windows Server 2016" ;SearchString="Cumulative Update for Windows Server 2016 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2016 for x64-based Systems"           ; ID="Windows Server 2016"}
-#$Products+=@{Product="Windows 10 20H2"     ;SearchString="Cumulative Update for Windows 10 Version 20H2 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 20H2 for x64-based Systems"       ; ID="Windows 10, version 1903 and later"}
 $Products+=@{Product="Windows 10 21H2"     ;SearchString="Cumulative Update for Windows 10 Version 1909 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 21H2 for x64-based Systems"       ; ID="Windows 10, version 1903 and later"}
 
 #grab folder to download to
 $folder=Read-Host -Prompt "Please type path to download. For example `"c:\windows\temp`" (if nothing specified, c:\windows\temp\_Updates_ is used)"
 if(!$folder){$folder='c:\windows\temp\_Updates_'}
 
-<#
-#do you want preview?
-$preview=Read-Host -Prompt "Do you want to download preview updates? y/n, default n"
-if($preview -eq "y"){
-    $preview = $true
-}else{
-    $preview=$false
-}
-#>
+#########################################################################################
 
 #let user choose products
 $SelectedProducts=$Products.Product | Out-GridView -OutputMode Multiple -Title "Please select products to download Cumulative Updates and Servicing Stack Updates"
+
+#########################################################################################
 
 #region check/download MSCatalog module
 Write-verbose -message "Checking if MSCatalog PS Module is Installed" -verbose
@@ -49,7 +41,6 @@ Write-verbose -message "Checking if MSCatalog PS Module is Installed" -verbose
         Write-verbose -message "--> MSCatalog PS Module is Installed" -verbose
     }
 #endregion
-
 #region check/download msrcsecurityupdates module
 Write-verbose -message "Checking if msrcsecurityupdates PS Module is Installed" -verbose
     if (!(Get-InstalledModule -Name msrcsecurityupdates -ErrorAction Ignore)){
@@ -68,7 +59,6 @@ Write-verbose -message "Checking if msrcsecurityupdates PS Module is Installed" 
         Import-module msrcsecurityupdates
     }
 #endregion
-
 #region check/download kbupdate module
 Write-verbose -message "Checking if kbupdate PS Module is Installed" -verbose
     if (!(Get-InstalledModule -Name kbupdate -ErrorAction Ignore)){
@@ -86,6 +76,8 @@ Write-verbose -message "Checking if kbupdate PS Module is Installed" -verbose
     }
 #endregion
 
+#########################################################################################
+
 #Build-CVEReport - stolen by Brandon Stevens
 Function Build-CVEReport {
 ### Install the module from the PowerShell Gallery (must be run as Admin)
@@ -100,31 +92,14 @@ Get-MsrcCvrfDocument -ID "$((get-date).Year)-$(($culture).DateTimeFormat.GetAbbr
 }
 #Build-CVEReport
 
+#########################################################################################
+
 #Folder
-    $DestinationFolder="$folder\$SelectedProduct\'$(Get-Date -Format 'yyyy-MM')'"
+    $DestinationFolder="$folder\$(Get-Date -Format 'yyyy-MM')"
     New-Item -Path $folder -ItemType Directory -ErrorAction Ignore | Out-Null
     New-Item -Path $DestinationFolder -ItemType Directory -ErrorAction Ignore | Out-Null
 
-<#
-#region download products
-Foreach($SelectedProduct in $SelectedProducts){
-    $item=$Products | Where-Object product -eq $SelectedProduct
-    #Download CU
-        If ($preview){
-            $update=Get-MSCatalogUpdate -Search $item.searchstring | Where-Object Products -eq $item.ID | Select-Object -First 1
-        }else{
-            $update=Get-MSCatalogUpdate -Search $item.searchstring | Where-Object Products -eq $item.ID | Where-Object Title -like "*$($item.SearchString)*" | Select-Object -First 1
-        }
-    Write-verbose -message "Downloading $($update.title) to $destinationFolder" -verbose
-    $update | Save-MSCatalogUpdate -Destination "$DestinationFolder" #-UseBits
-
-    #Download SSU
-    $update=Get-MSCatalogUpdate -Search $item.SSUSearchString | Where-Object Products -eq $item.ID | Select-Object -First 1
-    Write-verbose -message "Downloading $($update.title) to $destinationFolder"  -verbose
-    $update | Save-MSCatalogUpdate -Destination $DestinationFolder #-UseBits
-}
-#endregion
-#>
+#########################################################################################
 
 function Find-CumulativMicrosoftUpdateLatest2016
 {
@@ -153,6 +128,8 @@ function Find-CumulativMicrosoftUpdateLatest2016
   )
 }
 
+#########################################################################################
+
 function Find-CumulativMicrosoftUpdateLatest2019
 {
   Param(
@@ -178,6 +155,8 @@ function Find-CumulativMicrosoftUpdateLatest2019
      }
   )
 }
+
+#########################################################################################
 
 function Find-CumulativMicrosoftUpdateLatest2022
 {
@@ -205,30 +184,86 @@ function Find-CumulativMicrosoftUpdateLatest2022
   )
 }
 
-Write-Verbose -Message "---- KBList_2016 ----" -Verbose
+#########################################################################################
+
+# Download
+
+#Write-Verbose -Message "---- KBList_2016 ----" -Verbose
 #$KBList2016 = (Find-CumulativMicrosoftUpdateLatest2016 -Month '2022-08' | Select-Object -First 1).HotFixID
 $KBList2016 = (Find-CumulativMicrosoftUpdateLatest2016 -Month "$(Get-Date -Format 'yyyy-MM')" | Select-Object -First 1).HotFixID
-$KBList2016 
-Write-Verbose -Message "---- Latest Cumulative Update KB-Number: $KBList2016 ----" -Verbose
+#$KBList2016 
+Write-Verbose -Message "---> Download latest Cumulative Update KB-Number: $KBList2016 ----" -Verbose
 #Save-KbUpdate -Name $KBList2016 -FilePath $destinationFolder\$KBList2016.msu
+Save-KbUpdate -Name $KBList2016 
 
-Write-Verbose -Message "---- KBList_2019 ----" -Verbose
+#########################################################################################
+
+#Write-Verbose -Message "---- KBList_2019 ----" -Verbose
 $KBList2019 = (Find-CumulativMicrosoftUpdateLatest2019 -Month "$(Get-Date -Format 'yyyy-MM')" | Select-Object -First 1).HotFixID
-$KBList2019 
-Write-Verbose -Message "---- Latest Cumulative Update KB-Number: $KBList2019 ----" -Verbose
-#Save-KbUpdate -Name $KBList2019 -FilePath $destinationFolder\$KBList2019.msu
+#$KBList2019 
+Write-Verbose -Message "---> Download latest Cumulative Update KB-Number: $KBList2019 ----" -Verbose
+Save-KbUpdate -Name $KBList2019 -FilePath $destinationFolder\$KBList2019.msu
+Save-KbUpdate -Name $KBList2019 #-FilePath $destinationFolder\$KBList2019.msu
 
-Write-Verbose -Message "---- KBList_2022 ----" -Verbose
+#########################################################################################
+
+#Write-Verbose -Message "---- KBList_2022 ----" -Verbose
 $KBList2022 = (Find-CumulativMicrosoftUpdateLatest2022 -Month "$(Get-Date -Format 'yyyy-MM')" | Select-Object -First 1).HotFixID
-$KBList2022 
-Write-Verbose -Message "---- Latest Cumulative Update KB-Number: $KBList2022 ----" -Verbose
-Save-KbUpdate -Name $KBList2022 -FilePath $destinationFolder+$($KBList2022).msu
-#Save-KbUpdate -Name $KBList2022 -FilePath $destinationFolder
+#$KBList2022 
+Write-Verbose -Message "---> Download latest Cumulative Update KB-Number: $KBList2022 ----" -Verbose
+#Save-KbUpdate -Name $KBList2022 -FilePath c:\Windows\temp\KBList2022.msu
+Save-KbUpdate -Name $KBList2022
+
+#########################################################################################
 
 #start $destinationFolder
 start $folder
+
+#########################################################################################
 
 Write-verbose -message "Job finished" -verbose
 #Write-verbose -message "Press enter to continue" -verbose
 #Read-Host
 
+Stop-Transcript
+
+
+
+
+
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+
+
+<#
+#region download products
+Foreach($SelectedProduct in $SelectedProducts){
+    $item=$Products | Where-Object product -eq $SelectedProduct
+    #Download CU
+        If ($preview){
+            $update=Get-MSCatalogUpdate -Search $item.searchstring | Where-Object Products -eq $item.ID | Select-Object -First 1
+        }else{
+            $update=Get-MSCatalogUpdate -Search $item.searchstring | Where-Object Products -eq $item.ID | Where-Object Title -like "*$($item.SearchString)*" | Select-Object -First 1
+        }
+    Write-verbose -message "Downloading $($update.title) to $destinationFolder" -verbose
+    $update | Save-MSCatalogUpdate -Destination "$DestinationFolder" #-UseBits
+
+    #Download SSU
+    $update=Get-MSCatalogUpdate -Search $item.SSUSearchString | Where-Object Products -eq $item.ID | Select-Object -First 1
+    Write-verbose -message "Downloading $($update.title) to $destinationFolder"  -verbose
+    $update | Save-MSCatalogUpdate -Destination $DestinationFolder #-UseBits
+}
+#endregion
+#>
+
+<#
+#do you want preview?
+$preview=Read-Host -Prompt "Do you want to download preview updates? y/n, default n"
+if($preview -eq "y"){
+    $preview = $true
+}else{
+    $preview=$false
+}
+#>
