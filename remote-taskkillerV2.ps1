8261ffcad50b71b0db1277fc0bb7ffc5040950e7
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2023-02-07 15:57:40 +0100

    Update remote-taskkillerV2.ps1

--- remote-taskkillerV2.ps1
+++ remote-taskkillerV2.ps1
@@ -28,12 +28,22 @@ function Show-Taskkiller {
         [string]$computername
     )
 
+    begin{
+
+        $DateScanned = Get-Date -Format u
+        Write-Information -InformationAction Continue -MessageData ("Started Get-Processes at {0}" -f $DateScanned)
+
+        $stopwatch = New-Object System.Diagnostics.Stopwatch
+        $stopwatch.Start()
+    }
+
    process{
 
-        $ProcessArray = Get-Process -IncludeUserName
-        $CIMProcesses = Get-CimInstance -class win32_Process
-        $CIMServices = Get-CIMinstance -class Win32_Service        
-        $PerfProcArray = Get-CIMinstance -class Win32_PerfFormattedData_PerfProc_Process
+        #$ProcessArray = Get-Process -computername $computername #-IncludeUserName 
+        $ProcessArray = Invoke-Command -ComputerName  $computername -ScriptBlock {Get-Process -IncludeUserName }
+        $CIMProcesses = Get-CimInstance -class win32_Process -computername $computername  
+        $CIMServices = Get-CIMinstance -class Win32_Service -computername $computername  
+        $PerfProcArray = Get-CIMinstance -class Win32_PerfFormattedData_PerfProc_Process -computername $computername  
 
         foreach ($Process in $ProcessArray){
             
@@ -45,7 +55,7 @@ function Show-Taskkiller {
             $MemoryMB = $PerfProcArray | Where-Object IDProcess -eq $Process.ID | Select-Object -ExpandProperty workingSetPrivate
             $MemoryMB = try {[Math]::Round(($MemoryMB / 1mb),2)} Catch{}
 
-            $Process | Add-Member -MemberType NoteProperty -Name "Host" -Value $env:COMPUTERNAME
+            $Process | Add-Member -MemberType NoteProperty -Name "Host" -Value $computername
             $Process | Add-Member -MemberType NoteProperty -Name "DateScanned" -Value $DateScanned
             $Process | Add-Member -MemberType NoteProperty -Name "CommandLine" -Value $CommandLine
             $Process | Add-Member -MemberType NoteProperty -Name "Services" -Value $Services
@@ -55,12 +65,13 @@ function Show-Taskkiller {
             $Process | Add-Member -MemberType NoteProperty -Name "ThreadCount" -Value @($Process.Threads).Count
         }
 
-           $Processes =  $ProcessArray | Select-Object Host, DateScanned, CommandLine, Services, UserName, PercentProcessorTime, MemoryMB, BasePriority, Id, MainWindowHandle, MainWindowTitle, PriorityBoostEnabled, PriorityClass, PrivateMemorySize, PrivilegedProcessorTime, ProcessName, Responding, SessionId, StartTime, TotalProcessorTime, UserProcessorTime, Company, CPU, Description, FileVersion, Path, Product, ProductVersion, ModuleCount, ThreadCount, HandleCount | Out-GridView -PassThru  -Title "$($perf.PSComputerName) - Select process to kill"
-           Invoke-Command -ComputerName  svacxa -ScriptBlock {param($Processes) $Processes | ForEach-Object {Stop-Process -Id $Processes.ProcessId -force  }} -ArgumentList $Processes 
+           $Processes =  $ProcessArray | Select-Object Host, DateScanned, Product, Description, ProcessName, CommandLine, Services, UserName, StartTime, CPU, PercentProcessorTime, MemoryMB, BasePriority, Id, PriorityBoostEnabled, PriorityClass, PrivateMemorySize, PrivilegedProcessorTime, Responding, SessionId, TotalProcessorTime, UserProcessorTime, Company, FileVersion, Path, ProductVersion, ModuleCount, ThreadCount, MainWindowHandle, HandleCount | Out-GridView -PassThru  -Title "$($perf.PSComputerName) - Select process to kill"
+           Invoke-Command -ComputerName  $computername -ScriptBlock {param($Processes) $Processes | ForEach-Object {Stop-Process -Id $Processes.ProcessId -force  }} -ArgumentList $Processes 
 
     }
 
-}
+
+    }
 
 #Show-Taskkiller -computername localhost
-#Show-Taskkiller -computername remotecomputer
+Show-Taskkiller -computername svacxadmp1
